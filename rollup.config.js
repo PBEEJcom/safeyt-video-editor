4fbbb8913773e90df38c396b6b30ade6c3a492c3
Amos Endersen <amenders@iupui.edu>
2023-06-21 22:01:00 -0400

    functioning rollup packaging

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,25 @@
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import url from "@rollup/plugin-url";
-import json from "@rollup/plugin-json";
-import postcss from "rollup-plugin-postcss";
- 
-const { readdirSync, statSync } = require("fs");
-const { join } = require("path");
- 
-const dirs = p =>
- readdirSync(p).filter(f => statSync(join(p, f)).isDirectory());
- 
-const modules = dirs("src");
- 
-const config = moduleDir => ({
- input: `src/${moduleDir}/index.js`,
- output: [
-   {
-     file: `src/${moduleDir}/build/index.es.js`,
-     format: "es",
-     sourcemap: true
-   }
- ],
- external: ["react", "react-dom"],
- plugins: [
-   external(),
-   url(),
-   json(),
-   babel({
-     exclude: "node_modules/**",
-     presets: ["@babel/preset-react"],
-     plugins: [
-       "@babel/plugin-proposal-nullish-coalescing-operator"
-     ],
-     babelHelpers: "bundled"
-   }),
-   resolve({
-     browser: true
-   }),
-   commonjs(),
-   postcss({ plugins: [] })
- ]
-});
- 
-export default commandLineArgs => {
- if (commandLineArgs.hasOwnProperty("package"))
-   return [config(commandLineArgs.package)];
- return modules.map(m => config(m));
-};
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+
+const config = {
+    input: 'src/index.tsx',
+    output: {
+            dir: "dist",
+            format: 'esm',
+            sourcemap: true
+        },
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        postcss(),
+        terser()
+    ]
+}
+
+export default config;
