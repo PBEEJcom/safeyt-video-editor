eac42f22578065be37d6b4f18e74cda345e5a80c
Amos Endersen <amenders@iupui.edu>
2023-11-14 22:30:23 -0500

    fix css nesting issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,8 +36,8 @@ module.exports = {
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     }
-  }
-  // plugins: [
+  },
+  plugins: [
   //   require('tailwind-scrollbar-hide'),
-  // ],
+  ],
 }
